ef1287600d88d11f649f12e3498791c7f9b0ceb6
xcintas <xcintas@gmail.com>
2015-10-20 01:57:35 +0300

    last test fix

--- js/test.js
+++ js/test.js
@@ -4,6 +4,10 @@ describe("A suite", function() {
   it("contains spec with an expectation", function() {
     expect(true).toBe(true);
   });
+
+  it("true is true", function() {
+    expect(true).toBe(true);
+  });
 });
 
 
